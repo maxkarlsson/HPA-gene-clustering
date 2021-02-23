f51a649b17c6c9b71c93b4ddede8ecfbf727032a
María Bueno Álvez <buenoalvezm@gmail.com>
2021-02-23 08:13:34 +0100

    Update eval function

--- scripts/functions_evaluation.R
+++ scripts/functions_evaluation.R
@@ -1,17 +1,32 @@
 
+# Create random clustering
+
+
 # Evaluation 
 
-eval <- function(clustering) {
+eval <- function(clustering, genes, distance, id = NULL) { # add distance
+  dplyr::rename(clustering, cluster = value)
+  
+  r <- 
+    clustering %>%
+    left_join(genes, by = c("gene" = "enssscg_id")) %>%
+    na.omit() %>%
+    dplyr::select(-gene) %>%
+  
+  
+  clusters <- as.numeric(as.character(r$cluster))
+  
   con_index <-
-    connectivity(clustering)
+    connectivity(distance = distance, cluster = clustering$cluster)
+  
   dunn <- 
-    dunn()
+    dunn(distance = distance, cluster = clustering$cluster)
   
   bio_index <- 
     if(require("Biobase") && require("annotate") && require("GO.db") &&
-     require("moe430a.db")) {
-    BHI(cluster, annotation="moe430a.db", names=rownames(clustering), category="all")
-  }
+       require("org.Hs.eg.db")) {
+      BHI(clusters, annotation="org.Hs.eg.db", names=r$entrez, category="all") 
+    }
   
   list(connectivity = con_index,
        dunn = dunn_index,
