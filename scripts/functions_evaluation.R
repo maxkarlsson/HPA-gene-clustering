f8ea268e29204dc847db98eb1cc098394d059425
María Bueno Álvez <buenoalvezm@gmail.com>
2021-03-02 14:13:04 +0100

    Updated evaluation function

--- scripts/functions_evaluation.R
+++ scripts/functions_evaluation.R
@@ -8,15 +8,17 @@ random_cluster <- function (k,l, names) {
 
 # Transform results to dataframe
 clusters_to_df <- function (res_list, gene_names) {
-  df <- data.frame(id,gene,cluster)
-  for res in res_list {
-    new_entries <- data.frame(id = res$id, gene =  gene_names, cluster = res$cluster)
-    df <- bind_rows(df, new_entries)
+  df <- data.frame()
+  
+  for (cluster_method in res_list) {
+    for (res in cluster_method) {
+      new_entries <- data.frame(id = res$id, gene =  gene_names, 
+                                cluster = as.numeric(as.character(res$cluster$value)))
+      df <- bind_rows(df, new_entries)
+    }
   }
   return(df)
 }
-
-
 
 # Evaluation 
 
@@ -24,20 +26,30 @@ eval <- function(clustering, genes, dist, id = NULL) { # add distance
   
   #clustering <- dplyr::rename(clustering, cluster = value)
   
+  if (grepl("zscore euclidean",id)){d <- dist[[1]]$distance}
+  if (grepl("min-max euclidean",id)){d <- dist[[2]]$distance}
+  if (grepl("max euclidean",id)){d <- dist[[3]]$distance}
+  if (grepl("zscore manhattan",id)){d <- dist[[4]]$distance}
+  if (grepl("min-max manhattan",id)){d <- dist[[5]]$distance}
+  if (grepl("max manhattan",id)){d <- dist[[6]]$distance}
+  if (grepl("zscore pearson",id)){d <- dist[[7]]$distance}
+  if (grepl("min-max pearson",id)){d <- dist[[8]]$distance}
+  if (grepl("max pearson",id)){d <- dist[[9]]$distance}
+  
   r <- 
     clustering %>%
+    select(gene,cluster) %>%
     left_join(genes, by = c("gene" = "enssscg_id")) %>%
     na.omit() %>%
     dplyr::select(-gene) 
   
-  
   clusters <- as.numeric(as.character(r$cluster))
   
   con_index <-
-    connectivity(distance = dist, cluster = as.numeric(as.character(clustering$cluster)))
+    connectivity(distance = d, cluster = as.numeric(as.character(clustering$cluster)))
   
   dunn_index <- 
-    dunn(distance = dist, cluster = as.numeric(as.character(clustering$cluster)))
+    dunn(distance = d, cluster = as.numeric(as.character(clustering$cluster)))
   
   bio_index <- 
     if(require("Biobase") && require("annotate") && require("GO.db") &&
@@ -47,20 +59,16 @@ eval <- function(clustering, genes, dist, id = NULL) { # add distance
 
   
   if(!is.null(id)) {
-    return(data.frame(connectivity = con_index,
-                dunn = dunn_index,
-                BHI = bio_index,
-                id = id))
-    
-    #return(list(connectivity = con_index,
-    #           dunn = dunn_index,
-    #          BHI = bio_index,
-    #         id = id))
+    return(data.frame(id = id,
+                      connectivity = con_index,
+                      dunn = dunn_index,
+                      BHI = bio_index))
   }
+  
   else {
     return(data.frame(connectivity = con_index,
-                dunn = dunn_index,
-                BHI = bio_index))
+                      dunn = dunn_index,
+                      BHI = bio_index))
   }
 
 }
