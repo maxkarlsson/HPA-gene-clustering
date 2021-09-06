144fcf7a7f9121902df2a13755c5ed012e9801c0
María Bueno Álvez <buenoalvezm@gmail.com>
2021-09-06 14:53:26 +0200

    Updated folder structure function

--- scripts/functions_utility.R
+++ scripts/functions_utility.R
@@ -353,3 +353,45 @@ makeHexData <- function(x, y, z, bins, bin_fun) {
     mutate(z = tapply(z, h@cID, FUN = bin_fun),
            cid = h@cell)
 }
+
+
+create_folder_structure <-
+  function(dataset_id, dataset_run_id, main_folder, folders) {
+    dir.create(main_folder, showWarnings = FALSE)
+    dataset_folder <-
+      tibble(dataset_id,
+             dataset_run_id,
+             dataset_path = paste0(main_folder, "/",
+                                   dataset_id, " ",
+                                   dataset_run_id))
+    for(path in dataset_folder$dataset_path) dir.create(path, showWarnings = FALSE)
+    folders_df <-
+      enframe(folders,
+              "name",
+              "path") %>%
+      expand_grid(dataset_id) %>%
+      left_join(tibble(dataset_id,
+                       dataset_run_id),
+                by = "dataset_id") %>%
+      left_join(dataset_folder,
+                by = c("dataset_id",
+                       "dataset_run_id")) %>%
+      mutate(full_path = paste(dataset_path, path, sep = "/"))
+    for(path in folders_df$full_path) dir.create(path, showWarnings = FALSE)
+    out_paths <-
+      set_names(dataset_folder$dataset_id,
+                dataset_folder$dataset_id) %>%
+      lapply(function(x) {
+        clustering_info <-
+          dataset_folder %>%
+          filter(dataset_id == x)
+        temp <-
+          folders_df %>%
+          filter(dataset_id == x)
+        list(main = clustering_info$dataset_path,
+             run_id = clustering_info$dataset_run_id) %>%
+          append(split(temp$full_path,
+                       temp$name))
+      })
+    return(out_paths)
+  }
