d1ac8800dfd529e652d414e6582a981dde16ff61
maxkarlsson <40792056+maxkarlsson@users.noreply.github.com>
2021-11-09 11:43:31 +0100

    Improved polygon calculation

--- scripts/cluster rasterizer.R
+++ scripts/cluster rasterizer.R
@@ -29,6 +29,7 @@ get_density <-
                 by = "y")
   }
 
+
 generate_cluster_hulls <- 
   function(V1,
            V2, 
@@ -39,14 +40,22 @@ generate_cluster_hulls <-
            frac_lim = 0.05,
            plot_range = c(range(V1),
                           range(V2)) * 1.05,
-           bandwidth_fct = 1) {
+           poly_concavity = 1,
+           poly_smoothing = 1,
+           relative_bandwidth = 1/200) {
+    require(tidyverse)
+    require(magrittr)
+    require(sf)
+    require(sp)
+    require(concaveman)
     
+    # Compile indata
     cluster_data <- 
       tibble(V1, V2, 
              element_id,
              cluster = cluster_membership)
     
-    
+    # Calculate plot range
     plot_range_tb <- 
       set_names(plot_range,
                 c("xmin", 
@@ -56,45 +65,52 @@ generate_cluster_hulls <-
       enframe() %>% 
       spread(name, value)
     
-    plot_area <- 
-      (plot_range_tb$xmax - plot_range_tb$xmin) * 
-      (plot_range_tb$ymax - plot_range_tb$ymin)
+    # Calculate diagonal length
+    plot_diagonal <- 
+      sqrt((plot_range_tb$xmax - plot_range_tb$xmin)^2 + 
+             (plot_range_tb$ymax - plot_range_tb$ymin)^2)
     
+    # Set bandwidth to fraction of diagonal
     plot_bandwidth <- 
-      bandwidth_fct * plot_area / 1000
+      relative_bandwidth * plot_diagonal
     
     # Find subclusters
-    plot_data_sub <-
+    subclusters <-
       cluster_data %>%
       group_by(cluster) %>%
-      mutate(sub_cluster = data.frame(V1, V2) %>%
+      mutate(n_cluster_genes = n_distinct(element_id), 
+             sub_cluster = data.frame(V1, V2) %>%
                fpc::dbscan(eps = plot_bandwidth) %$%
                cluster) %>%
       ungroup() %>% 
       group_by(cluster, sub_cluster) %>%
       mutate(n_sub_genes = n_distinct(element_id)) %>% 
-      ungroup() %>%
-      left_join(select(., cluster, sub_cluster, n_sub_genes) %>% 
-                  distinct() %>%
-                  group_by(cluster) %>%
-                  mutate(sub_type = case_when(sub_cluster == 0 ~ "outlier",
-                                              n_sub_genes / sum(n_sub_genes) < frac_lim ~ "outlier",
-                                              rank(-n_sub_genes, 
-                                                   ties.method = "first") == 1 ~ "primary",
-                                              T ~ "secondary")),
-                by = c("cluster", "sub_cluster", "n_sub_genes")) %>% 
-      ungroup() %>% 
-      arrange(cluster)
+      ungroup() 
+    
+    # Classify subclusters
+    subclusters_classes <- 
+      subclusters %>% 
+      select(cluster, sub_cluster, n_cluster_genes, n_sub_genes) %>% 
+      distinct() %>% 
+      group_by(cluster) %>%
+      mutate(sub_type = case_when(sub_cluster == 0 ~ "outlier",
+                                  n_sub_genes / n_cluster_genes < frac_lim ~ "outlier",
+                                  rank(-n_sub_genes, 
+                                       ties.method = "first") == 1 ~ "primary",
+                                  T ~ "secondary")) %>% 
+      select(cluster, sub_cluster, sub_type)
+    
+    subclusters_classed <- 
+      subclusters %>% 
+      left_join(subclusters_classes,
+                by = c("cluster", "sub_cluster"))
     
     
     # Calculate plot density
     plot_density <- 
-      plot_data_sub %>% 
+      subclusters_classed %>% 
       filter(sub_type != "outlier") %>%
-      bind_rows("primary secondary" = ., 
-                "primary" = filter(., sub_type == "primary"),
-                .id = "density_type") %>%
-      group_by(density_type, cluster, sub_cluster) %>%
+      group_by(cluster, sub_cluster) %>%
       do({
         get_density(.$V1, 
                     .$V2, 
@@ -105,7 +121,7 @@ generate_cluster_hulls <-
       }) %>% 
       ungroup() %>% 
       filter(z > 1e-200) %>% 
-      group_by(density_type, cluster, sub_cluster) %>% 
+      group_by(cluster, sub_cluster) %>% 
       mutate(z = z / sum(z)) %>% 
       arrange(cluster, sub_cluster, -z) %>% 
       mutate(cum_z = cumsum(z)) %>% 
@@ -117,68 +133,84 @@ generate_cluster_hulls <-
     plot_density_filtered <- 
       plot_density %>% 
       filter(cum_z < cum_z_lim) %>%
-      group_by(density_type, x, y) %>% 
+      group_by(x, y) %>% 
       top_n(1, z) %>%
       slice(1) %>% 
       ungroup()
     
     
-    
-    
-    # Define a main landmass
+    # Calculate size of landmass
     plot_density_landmass <-
       plot_density_filtered %>%
-      group_by(density_type, cluster, sub_cluster) %>%
+      group_by(cluster, sub_cluster) %>%
       mutate(landmass = data.frame(x_coord, y_coord) %>%
-               fpc::dbscan(eps = plot_bandwidth / 2.5) %$%
+               fpc::dbscan(eps = plot_bandwidth) %$%
                cluster) %>%
-      group_by(density_type, cluster, sub_cluster, landmass) %>% 
+      group_by(cluster, sub_cluster, landmass) %>% 
       mutate(n_landmass_points = length(x)) %>%  
-      ungroup() %>%
-      left_join(select(., density_type, cluster, sub_cluster, landmass, n_landmass_points) %>% 
-                  distinct() %>%
-                  group_by(density_type, cluster, sub_cluster) %>%
-                  
-                  mutate(frac_landmass = n_landmass_points / sum(n_landmass_points),
-                         landmass_type = case_when(rank(-n_landmass_points, 
-                                                        ties.method = "first") == 1 ~ "primary",
-                                                   T ~ "secondary"))) %>% 
       ungroup() %>% 
+      group_by(cluster) %>%
+      mutate(n_total_points = length(x)) %>% 
+      ungroup() 
+    
+    # Classify landmasses
+    plot_density_landmass_classes <- 
+      plot_density_landmass %>% 
+      select(cluster, sub_cluster, landmass, n_landmass_points, n_total_points) %>% 
+      distinct() %>%
+      group_by(cluster, sub_cluster) %>%
+      
+      mutate(frac_landmass = n_landmass_points / n_total_points,
+             landmass_type = case_when(rank(-n_landmass_points, 
+                                            ties.method = "first") == 1 ~ "primary",
+                                       T ~ "secondary")) %>% 
+      ungroup() %>% 
+      select(cluster, sub_cluster, landmass, landmass_type, frac_landmass)
+    
+    plot_density_landmass_classed <- 
+      plot_density_landmass %>%
+      left_join(plot_density_landmass_classes,
+                by = c("cluster", 
+                       "sub_cluster",
+                       "landmass")) %>% 
       arrange(cluster)
     
-    
-    
-    
     plot_density_mainland_filtered <-
-      plot_density %>% 
-      left_join(plot_density_landmass %>% 
-                  select(density_type, cluster, sub_cluster, x, y, landmass, frac_landmass, landmass_type)) %>%
-      # filter(landmass_type != "secondary") %>%
+      plot_density_landmass_classed %>% 
       filter(frac_landmass > frac_lim) %>%
       filter(cum_z < cum_z_lim) %>%
-      group_by(density_type, x, y) %>% 
+      group_by(x, y) %>% 
       top_n(1, z) %>%
       slice(1) %>% 
       ungroup()
     
     ######
+    
+    # Create polygons
+    # Poly smoothing: How small distances should be further detailed - 
+    # higher values --> less detailed
+    # poly concavity: How convex polygons should be - 
+    # higher values --> less detailed
+    
     plot_data_hulls <- 
       plot_density_mainland_filtered %>% 
       
-      group_by(density_type, cluster, sub_cluster, landmass) %>% 
+      group_by(cluster, sub_cluster, landmass) %>% 
       do({
         st_as_sf(., coords=c('x_coord','y_coord')) %>%
-          concaveman(concavity = 1, length_threshold = plot_bandwidth / 2) %$%
+          concaveman(concavity = poly_concavity, 
+                     length_threshold = plot_bandwidth * poly_smoothing) %$%
           st_coordinates(polygons) %>% 
           as_tibble()
       }) %>% 
-      ungroup()
+      ungroup() %>% 
+      mutate(polygon_id = paste(cluster, sub_cluster, landmass, sep = "_"))
     
     
-    list(data = plot_data_sub,
-         hulls = plot_data_hulls,
+    
+    list(hulls = plot_data_hulls,
          density = plot_density,
-         density_landmass = plot_density_mainland_filtered)
+         landmass_pixels = plot_density_mainland_filtered)
   }
 
 
